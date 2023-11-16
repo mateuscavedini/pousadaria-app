20b19b6638291d5c60507e49dbf709d0ebc6bef3
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2023-11-16 14:50:39 -0300

    Adiciona validações e testes unitários para model booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,16 @@
 class Booking < ApplicationRecord
   belongs_to :guest
   belongs_to :room
+
+  enum status: { pending: 0, ongoing: 2, finished: 4, canceled: 6 }
+
+  validates :start_date, :finish_date, :guests_number, presence: true
+  validates_with DatesAreFutureValidator, FinishDateIsLaterThanStartDateValidator, DatesAreAvailableValidator
+  validate :guests_number_respects_room_capacity
+
+  private
+
+  def guests_number_respects_room_capacity
+    self.errors.add(:guests_number, 'deve respeitar a capacidade máxima do quarto') if self.guests_number.present? && self.guests_number > self.room.max_capacity
+  end
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,99 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
-  pending '#valid?'
+  describe '#valid?' do
+    it 'inválido quando data inicial está vazia' do
+      booking = Booking.new(start_date: '')
+
+      booking.valid?
+
+      expect(booking.errors.include? :start_date).to be true
+    end
+
+    it 'inválido quando data final está vazia' do
+      booking = Booking.new(finish_date: '')
+
+      booking.valid?
+
+      expect(booking.errors.include? :finish_date).to be true
+    end
+
+    it 'inválido quando número de hóspedes está vazio' do
+      booking = Booking.new(guests_number: '')
+
+      booking.valid?
+
+      expect(booking.errors.include? :guests_number).to be true
+    end
+
+    it 'inválido quando data inicial é passada' do
+      booking = Booking.new(start_date: 1.day.ago)
+
+      booking.valid?
+
+      expect(booking.errors.include? :start_date).to be true
+    end
+
+    it 'inválido quando data final é passada' do
+      booking = Booking.new(finish_date: 1.day.ago)
+
+      booking.valid?
+
+      expect(booking.errors.include? :finish_date).to be true
+    end
+    
+    it 'inválido quando data final é anterior à data inicial' do
+      booking = Booking.new(start_date: 5.days.from_now, finish_date: 1.day.from_now)
+
+      booking.valid?
+
+      expect(booking.errors.include? :finish_date).to be true
+    end
+
+    it 'inválido quando já existe uma reserva pendente no período especificado' do
+      maria = Guest.create!(first_name: 'Maria', last_name: 'da Silva', social_number: '11122233345', email: 'maria@email.com', password: 'senha123')
+      owner = Owner.create!(name: 'Maria', email: 'maria@email.com', password: 'senha123')
+      address_attributes = { street_name: 'Rua do Teste', street_number: '100', district: 'Jd. Testando', city: 'Jundiaí', state: 'SP', postal_code: '11010-001' }
+      contact_attributes = { phone: '11912344321', email: 'pousada@teste.com' }
+      guesthouse = Guesthouse.create!(corporate_name: 'Pousadas Brasil LTDA', trading_name: 'Pousada Teste', registration_number: '12345678000100', description: 'Ambientes com Wi-Fi, suítes privadas, quartos compartilhados, segurança 24h.', allow_pets: false, usage_policy: 'Proibido fumar nos ambientes da pousada; Proibido barulho após as 22h.', check_in: '11:00', check_out: '10:30', payment_methods: 'Dinheiro e Cartão de Crédito', address_attributes: address_attributes, contact_attributes: contact_attributes, owner: owner)
+      room = Room.create!(name: 'Primeiro Quarto', description: 'Primeiro quarto a ser cadastrado', area: 50, max_capacity: 2, daily_rate: 100, has_bathroom: true, has_balcony: false, has_air_conditioner: true, has_tv: true, has_wardrobe: true, has_safe: true, is_accessible: true, guesthouse: guesthouse)
+
+      Booking.create!(start_date: 1.day.from_now, finish_date: 5.days.from_now, guests_number: 1, room: room, guest: maria, status: :pending)
+      booking = Booking.new(start_date: 2.days.from_now, finish_date: 6.days.from_now, room: room)
+
+      booking.valid?
+
+      expect(booking.errors.include? :base).to be true
+    end
+    
+    it 'inválido quando já existe uma reserva em andamento no período especificado' do
+      maria = Guest.create!(first_name: 'Maria', last_name: 'da Silva', social_number: '11122233345', email: 'maria@email.com', password: 'senha123')
+      owner = Owner.create!(name: 'Maria', email: 'maria@email.com', password: 'senha123')
+      address_attributes = { street_name: 'Rua do Teste', street_number: '100', district: 'Jd. Testando', city: 'Jundiaí', state: 'SP', postal_code: '11010-001' }
+      contact_attributes = { phone: '11912344321', email: 'pousada@teste.com' }
+      guesthouse = Guesthouse.create!(corporate_name: 'Pousadas Brasil LTDA', trading_name: 'Pousada Teste', registration_number: '12345678000100', description: 'Ambientes com Wi-Fi, suítes privadas, quartos compartilhados, segurança 24h.', allow_pets: false, usage_policy: 'Proibido fumar nos ambientes da pousada; Proibido barulho após as 22h.', check_in: '11:00', check_out: '10:30', payment_methods: 'Dinheiro e Cartão de Crédito', address_attributes: address_attributes, contact_attributes: contact_attributes, owner: owner)
+      room = Room.create!(name: 'Primeiro Quarto', description: 'Primeiro quarto a ser cadastrado', area: 50, max_capacity: 2, daily_rate: 100, has_bathroom: true, has_balcony: false, has_air_conditioner: true, has_tv: true, has_wardrobe: true, has_safe: true, is_accessible: true, guesthouse: guesthouse)
+
+      Booking.create!(start_date: 1.day.from_now, finish_date: 5.days.from_now, guests_number: 1, room: room, guest: maria, status: :ongoing)
+      booking = Booking.new(start_date: 2.days.from_now, finish_date: 6.days.from_now, room: room)
+
+      booking.valid?
+
+      expect(booking.errors.include? :base).to be true
+    end
+
+    it 'inválido quando número de hóspedes é superior à capacidade máxima do quarto' do
+      owner = Owner.create!(name: 'Maria', email: 'maria@email.com', password: 'senha123')
+      address_attributes = { street_name: 'Rua do Teste', street_number: '100', district: 'Jd. Testando', city: 'Jundiaí', state: 'SP', postal_code: '11010-001' }
+      contact_attributes = { phone: '11912344321', email: 'pousada@teste.com' }
+      guesthouse = Guesthouse.create!(corporate_name: 'Pousadas Brasil LTDA', trading_name: 'Pousada Teste', registration_number: '12345678000100', description: 'Ambientes com Wi-Fi, suítes privadas, quartos compartilhados, segurança 24h.', allow_pets: false, usage_policy: 'Proibido fumar nos ambientes da pousada; Proibido barulho após as 22h.', check_in: '11:00', check_out: '10:30', payment_methods: 'Dinheiro e Cartão de Crédito', address_attributes: address_attributes, contact_attributes: contact_attributes, owner: owner)
+      room = Room.create!(name: 'Primeiro Quarto', description: 'Primeiro quarto a ser cadastrado', area: 50, max_capacity: 2, daily_rate: 100, has_bathroom: true, has_balcony: false, has_air_conditioner: true, has_tv: true, has_wardrobe: true, has_safe: true, is_accessible: true, guesthouse: guesthouse)
+
+      booking = Booking.new(guests_number: 5, room: room)
+
+      booking.valid?
+
+      expect(booking.errors.include? :guests_number).to be true
+    end
+  end
 end
