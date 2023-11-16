ad32c4149ee987cfc3280d9a289fa6aa194f3a16
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2023-11-16 03:17:31 -0300

    Adicionado testes para guest

--- spec/system/guests/guest_signs_in_spec.rb
+++ spec/system/guests/guest_signs_in_spec.rb
@@ -1,6 +1,45 @@
 require 'rails_helper'
 
 describe 'Hóspede se autentica' do
-  pending 'com sucesso'
-  pending 'e faz logout'
+  it 'com sucesso' do
+    Guest.create!(first_name: 'Maria', last_name: 'da Silva', social_number: '11122233345', email: 'maria@email.com', password: 'senha123')
+
+    visit root_path
+    within '#guest-session-links' do
+      click_on 'Entrar'
+    end
+    fill_in 'E-mail', with: 'maria@email.com'
+    fill_in 'Senha', with: 'senha123'
+    within 'main form' do
+      click_on 'Entrar'
+    end
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Login efetuado com sucesso.'
+    within 'header nav' do
+      expect(page).to have_content 'Maria da Silva'
+      expect(page).to have_button 'Sair'
+      expect(page).not_to have_link 'Registrar'
+      expect(page).not_to have_link 'Entrar'
+    end
+  end
+
+  it 'e faz logout' do
+    guest = Guest.create!(first_name: 'Maria', last_name: 'da Silva', social_number: '11122233345', email: 'maria@email.com', password: 'senha123')
+
+    login_as guest, scope: :guest
+    visit root_path
+    within 'header nav' do
+      click_on 'Sair'
+    end
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    within 'header nav' do
+      expect(page).to have_link 'Registrar'
+      expect(page).to have_link 'Entrar'
+      expect(page).not_to have_content 'Maria da Silva'
+      expect(page).not_to have_button 'Sair'
+    end
+  end
 end
--- spec/system/guests/user_signs_up_as_guest_spec.rb
+++ spec/system/guests/user_signs_up_as_guest_spec.rb
@@ -1,7 +1,77 @@
 require 'rails_helper'
 
 describe 'Usuário cria conta como hóspede' do
-  pending 'a partir do menu'
-  pending 'com sucesso'
-  pending 'com dados insuficientes'
+  it 'a partir do menu' do
+    visit root_path
+    within '#guest-session-links' do
+      click_on 'Registrar'
+    end
+
+    expect(page).to have_field 'Nome'
+    expect(page).to have_field 'Sobrenome'
+    expect(page).to have_field 'CPF'
+    expect(page).to have_field 'E-mail'
+    expect(page).to have_field 'Senha'
+    expect(page).to have_field 'Confirme sua senha'
+    expect(page).to have_button 'Criar Conta'
+  end
+
+  it 'com sucesso' do
+    visit root_path
+    within '#guest-session-links' do
+      click_on 'Registrar'
+    end
+    fill_in 'Nome', with: 'Maria'
+    fill_in 'Sobrenome', with: 'da Silva'
+    fill_in 'CPF', with: '11122233345'
+    fill_in 'E-mail', with: 'maria@email.com'
+    fill_in 'Senha', with: 'senha123'
+    fill_in 'Confirme sua senha', with: 'senha123'
+    click_on 'Criar Conta'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Boas vindas! Você realizou seu registro com sucesso.'
+    within '#header-links' do
+      expect(page).to have_content 'Maria da Silva'
+      expect(page).to have_button 'Sair'
+      expect(page).not_to have_content 'Hóspede'
+      expect(page).not_to have_content 'Proprietário'
+      expect(page).not_to have_link 'Registrar'
+      expect(page).not_to have_link 'Entrar'
+    end
+  end
+
+  it 'com dados insuficientes' do
+    visit root_path
+    within '#guest-session-links' do
+      click_on 'Registrar'
+    end
+    fill_in 'Nome', with: ''
+    fill_in 'Sobrenome', with: ''
+    fill_in 'CPF', with: ''
+    fill_in 'E-mail', with: ''
+    fill_in 'Senha', with: ''
+    click_on 'Criar Conta'
+
+    expect(page).to have_content 'Não foi possível salvar hóspede'
+    expect(page).to have_content 'Nome não pode ficar em branco'
+    expect(page).to have_content 'Sobrenome não pode ficar em branco'
+    expect(page).to have_content 'CPF não pode ficar em branco'
+    expect(page).to have_content 'E-mail não pode ficar em branco'
+    expect(page).to have_content 'Senha não pode ficar em branco'
+  end
+
+  it 'com cpf já cadastrado' do
+    Guest.create!(first_name: 'Maria', last_name: 'da Silva', social_number: '11122233345', email: 'maria@email.com', password: 'senha123')
+
+    visit root_path
+    within '#guest-session-links' do
+      click_on 'Registrar'
+    end
+    fill_in 'CPF', with: '11122233345'
+    click_on 'Criar Conta'
+
+    expect(page).to have_content 'Não foi possível salvar hóspede'
+    expect(page).to have_content 'CPF já está em uso'
+  end
 end
