1377fc1bb33545f66b1470027e7650e8df3060f6
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2023-11-18 03:16:36 -0300

    Adicionado método para cálculo do total a pagar

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,4 +7,29 @@ class Room < ApplicationRecord
   enum status: { active: 0, inactive: 2 }
 
   validates :name, :description, :area, :max_capacity, :daily_rate, presence: true
+
+  def calculate_total_price(start_date, finish_date)
+    stay_range = Range.new(start_date.to_date, finish_date.to_date)
+    seasonal_rates = SeasonalRate.active.where(room_id: self.id)
+
+    return stay_range.count * self.daily_rate unless seasonal_rates.present?
+
+    total_price = 0
+    regular_days = stay_range.to_a
+
+    stay_range.each do |day|
+      seasonal_rates.each do |seasonal_rate|
+        seasonal_rate_range = Range.new(seasonal_rate.start_date.to_date, seasonal_rate.finish_date.to_date)
+
+        if seasonal_rate_range.include? day
+          total_price += seasonal_rate.rate
+          regular_days.delete(day)
+        end
+      end
+    end
+
+    return total_price unless regular_days.present?
+
+    total_price + (regular_days.count * self.daily_rate)
+  end
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -42,4 +42,58 @@ RSpec.describe Room, type: :model do
       expect(room.errors.include? :daily_rate).to be true
     end
   end
+
+  describe '#calculate_total_price' do
+    it 'em período sem preços sazonais' do
+      owner = Owner.create!(name: 'Maria', email: 'maria@email.com', password: 'senha123')
+      address_attributes = { street_name: 'Rua do Teste', street_number: '100', district: 'Jd. Testando', city: 'Jundiaí', state: 'SP', postal_code: '11010-001' }
+      contact_attributes = { phone: '11912344321', email: 'pousada@teste.com' }
+      guesthouse = Guesthouse.create!(corporate_name: 'Pousadas Brasil LTDA', trading_name: 'Pousada Teste', registration_number: '12345678000100', description: 'Ambientes com Wi-Fi, suítes privadas, quartos compartilhados, segurança 24h.', allow_pets: false, usage_policy: 'Proibido fumar nos ambientes da pousada; Proibido barulho após as 22h.', check_in: '11:00', check_out: '10:30', payment_methods: 'Dinheiro e Cartão de Crédito', address_attributes: address_attributes, contact_attributes: contact_attributes, owner: owner)
+      room = Room.create!(name: 'Primeiro Quarto', description: 'Primeiro quarto a ser cadastrado', area: 50, max_capacity: 2, daily_rate: 100, has_bathroom: true, has_balcony: false, has_air_conditioner: true, has_tv: true, has_wardrobe: true, has_safe: true, is_accessible: true, guesthouse: guesthouse)
+
+      result = room.calculate_total_price(1.day.from_now, 5.days.from_now)
+
+      expect(result).to eq 500
+    end
+
+    it 'em período com um preço sazonal' do
+      owner = Owner.create!(name: 'Maria', email: 'maria@email.com', password: 'senha123')
+      address_attributes = { street_name: 'Rua do Teste', street_number: '100', district: 'Jd. Testando', city: 'Jundiaí', state: 'SP', postal_code: '11010-001' }
+      contact_attributes = { phone: '11912344321', email: 'pousada@teste.com' }
+      guesthouse = Guesthouse.create!(corporate_name: 'Pousadas Brasil LTDA', trading_name: 'Pousada Teste', registration_number: '12345678000100', description: 'Ambientes com Wi-Fi, suítes privadas, quartos compartilhados, segurança 24h.', allow_pets: false, usage_policy: 'Proibido fumar nos ambientes da pousada; Proibido barulho após as 22h.', check_in: '11:00', check_out: '10:30', payment_methods: 'Dinheiro e Cartão de Crédito', address_attributes: address_attributes, contact_attributes: contact_attributes, owner: owner)
+      room = Room.create!(name: 'Primeiro Quarto', description: 'Primeiro quarto a ser cadastrado', area: 50, max_capacity: 2, daily_rate: 100, has_bathroom: true, has_balcony: false, has_air_conditioner: true, has_tv: true, has_wardrobe: true, has_safe: true, is_accessible: true, guesthouse: guesthouse)
+      SeasonalRate.create!(start_date: 4.days.from_now, finish_date: 5.days.from_now, rate: 85.50, room: room)
+
+      result = room.calculate_total_price(1.day.from_now, 5.days.from_now)
+
+      expect(result).to eq (3 * 100) + (2 * 85.50)
+    end
+
+    it 'em período com múltiplos preços sazonais' do
+      owner = Owner.create!(name: 'Maria', email: 'maria@email.com', password: 'senha123')
+      address_attributes = { street_name: 'Rua do Teste', street_number: '100', district: 'Jd. Testando', city: 'Jundiaí', state: 'SP', postal_code: '11010-001' }
+      contact_attributes = { phone: '11912344321', email: 'pousada@teste.com' }
+      guesthouse = Guesthouse.create!(corporate_name: 'Pousadas Brasil LTDA', trading_name: 'Pousada Teste', registration_number: '12345678000100', description: 'Ambientes com Wi-Fi, suítes privadas, quartos compartilhados, segurança 24h.', allow_pets: false, usage_policy: 'Proibido fumar nos ambientes da pousada; Proibido barulho após as 22h.', check_in: '11:00', check_out: '10:30', payment_methods: 'Dinheiro e Cartão de Crédito', address_attributes: address_attributes, contact_attributes: contact_attributes, owner: owner)
+      room = Room.create!(name: 'Primeiro Quarto', description: 'Primeiro quarto a ser cadastrado', area: 50, max_capacity: 2, daily_rate: 100, has_bathroom: true, has_balcony: false, has_air_conditioner: true, has_tv: true, has_wardrobe: true, has_safe: true, is_accessible: true, guesthouse: guesthouse)
+      SeasonalRate.create!(start_date: 2.days.from_now, finish_date: 3.days.from_now, rate: 85.50, room: room)
+      SeasonalRate.create!(start_date: 4.days.from_now, finish_date: 5.days.from_now, rate: 115.50, room: room)
+
+      result = room.calculate_total_price(1.day.from_now, 5.days.from_now)
+
+      expect(result).to eq 100 + (2 * 85.50) + (2 * 115.50)
+    end
+
+    it 'em período sem dias regulares' do
+      owner = Owner.create!(name: 'Maria', email: 'maria@email.com', password: 'senha123')
+      address_attributes = { street_name: 'Rua do Teste', street_number: '100', district: 'Jd. Testando', city: 'Jundiaí', state: 'SP', postal_code: '11010-001' }
+      contact_attributes = { phone: '11912344321', email: 'pousada@teste.com' }
+      guesthouse = Guesthouse.create!(corporate_name: 'Pousadas Brasil LTDA', trading_name: 'Pousada Teste', registration_number: '12345678000100', description: 'Ambientes com Wi-Fi, suítes privadas, quartos compartilhados, segurança 24h.', allow_pets: false, usage_policy: 'Proibido fumar nos ambientes da pousada; Proibido barulho após as 22h.', check_in: '11:00', check_out: '10:30', payment_methods: 'Dinheiro e Cartão de Crédito', address_attributes: address_attributes, contact_attributes: contact_attributes, owner: owner)
+      room = Room.create!(name: 'Primeiro Quarto', description: 'Primeiro quarto a ser cadastrado', area: 50, max_capacity: 2, daily_rate: 100, has_bathroom: true, has_balcony: false, has_air_conditioner: true, has_tv: true, has_wardrobe: true, has_safe: true, is_accessible: true, guesthouse: guesthouse)
+      SeasonalRate.create!(start_date: 1.day.from_now, finish_date: 5.days.from_now, rate: 85.50, room: room)
+
+      result = room.calculate_total_price(1.day.from_now, 5.days.from_now)
+
+      expect(result).to eq 5 * 85.50
+    end
+  end
 end
