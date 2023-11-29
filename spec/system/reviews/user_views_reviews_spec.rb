6991f11b2ba41b04cc8f204a6fdae6738e2739d5
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2023-11-29 03:53:56 -0300

    Hóspedes e Proprietários veem suas avaliações

--- spec/system/reviews/user_views_reviews_spec.rb
+++ spec/system/reviews/user_views_reviews_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
-describe 'Usuário vê todas as avaliações de uma pousada' do
-  pending 'a partir da página inicial'
-
-  pending 'e não existem avaliações'
-end
-
 describe 'Proprietário vê todas as avaliações de sua pousada' do
   it 'a partir do menu' do
     owner = Owner.create!(name: 'Maria', email: 'maria@email.com', password: 'senha123')
@@ -29,7 +23,7 @@ describe 'Proprietário vê todas as avaliações de sua pousada' do
       click_on 'Avaliações'
     end
 
-    expect(page).to have_content 'Avaliações - Pousada Teste'
+    expect(page).to have_content 'Minhas Avaliações'
     expect(page).to have_content "Reserva #{first_booking.code}"
     expect(page).to have_content 'Hóspede: Maria dos Santos'
     expect(page).to have_content 'Nota: 4'
@@ -42,5 +36,58 @@ describe 'Proprietário vê todas as avaliações de sua pousada' do
     expect(page).to have_content 'Hóspede: Luis Rodrigues'
     expect(page).to have_content 'Nota: 3'
     expect(page).to have_content 'Comentário: Uma boa pousada, mas com preços elevados.'
+    expect(page).not_to have_content 'Pousada Teste - Primeiro Quarto'
   end
+
+  it 'e não existem avaliações' do
+    owner = Owner.create!(name: 'Maria', email: 'maria@email.com', password: 'senha123')
+    address_attributes = { street_name: 'Rua do Teste', street_number: '100', district: 'Jd. Testando', city: 'Jundiaí', state: 'SP', postal_code: '11010-001' }
+    contact_attributes = { phone: '11912344321', email: 'pousada@teste.com' }
+    guesthouse = Guesthouse.create!(corporate_name: 'Pousadas Brasil LTDA', trading_name: 'Pousada Teste', registration_number: '12345678000100', description: 'Ambientes com Wi-Fi, suítes privadas, quartos compartilhados, segurança 24h.', allow_pets: false, usage_policy: 'Proibido fumar nos ambientes da pousada; Proibido barulho após as 22h.', check_in: '11:00', check_out: '10:30', payment_methods: 'Dinheiro e Cartão de Crédito', address_attributes: address_attributes, contact_attributes: contact_attributes, owner: owner)
+    room = Room.create!(name: 'Primeiro Quarto', description: 'Primeiro quarto a ser cadastrado', area: 50, max_capacity: 2, daily_rate: 100, has_bathroom: true, has_balcony: false, has_air_conditioner: true, has_tv: true, has_wardrobe: true, has_safe: true, is_accessible: true, guesthouse: guesthouse, status: :active)
+    guest = Guest.create!(first_name: 'Maria', last_name: 'dos Santos', social_number: '11122233345', email: 'maria@email.com', password: 'senha123')
+    Booking.create!(start_date: 1.day.from_now, finish_date: 5.days.from_now, guests_number: 1, total_price: room.calculate_total_price(1.day.from_now, 5.days.from_now), room: room, guest: guest, status: :finished)
+
+    login_as owner, scope: :owner
+    visit root_path
+    click_on 'Avaliações'
+
+    expect(page).to have_content 'Não existem avaliações a serem exibidas.'
+  end
+end
+
+describe 'Hóspede vê todas as suas avaliações' do
+  it 'e respectivas respostas do proprietário a partir do menu' do
+    owner = Owner.create!(name: 'Maria', email: 'maria@email.com', password: 'senha123')
+    address_attributes = { street_name: 'Rua do Teste', street_number: '100', district: 'Jd. Testando', city: 'Jundiaí', state: 'SP', postal_code: '11010-001' }
+    contact_attributes = { phone: '11912344321', email: 'pousada@teste.com' }
+    guesthouse = Guesthouse.create!(corporate_name: 'Pousadas Brasil LTDA', trading_name: 'Pousada Teste', registration_number: '12345678000100', description: 'Ambientes com Wi-Fi, suítes privadas, quartos compartilhados, segurança 24h.', allow_pets: false, usage_policy: 'Proibido fumar nos ambientes da pousada; Proibido barulho após as 22h.', check_in: '11:00', check_out: '10:30', payment_methods: 'Dinheiro e Cartão de Crédito', address_attributes: address_attributes, contact_attributes: contact_attributes, owner: owner)
+    first_room = Room.create!(name: 'Primeiro Quarto', description: 'Primeiro quarto a ser cadastrado', area: 50, max_capacity: 2, daily_rate: 100, has_bathroom: true, has_balcony: false, has_air_conditioner: true, has_tv: true, has_wardrobe: true, has_safe: true, is_accessible: true, guesthouse: guesthouse, status: :active)
+    second_room = Room.create!(name: 'Segundo Quarto', description: 'Segundo quarto a ser cadastrado', area: 30, max_capacity: 2, daily_rate: 50, has_bathroom: true, has_balcony: false, has_air_conditioner: false, has_tv: true, has_wardrobe: false, has_safe: false, is_accessible: true, guesthouse: guesthouse, status: :active)
+    guest = Guest.create!(first_name: 'Maria', last_name: 'dos Santos', social_number: '11122233345', email: 'maria@email.com', password: 'senha123')
+    first_booking = Booking.create!(start_date: 1.day.from_now, finish_date: 5.days.from_now, guests_number: 1, total_price: first_room.calculate_total_price(1.day.from_now, 5.days.from_now), room: first_room, guest: guest, status: :finished)
+    second_booking = Booking.create!(start_date: 6.days.from_now, finish_date: 10.days.from_now, guests_number: 2, total_price: second_room.calculate_total_price(6.days.from_now, 10.days.from_now), room: second_room, guest: guest, status: :finished)
+    first_review = Review.create!(rating: 5, comment: 'Ótima pousada e quarto! Recomendo!', reply: 'Agradecemos a avaliação!', booking: first_booking)
+    second_review = Review.create!(rating: 3, comment: 'Ótima pousada mas há quartos melhores.', booking: second_booking)
+
+    login_as guest, scope: :guest
+    visit root_path
+    click_on 'Minhas Avaliações'
+
+    expect(page).to have_content "Reserva #{first_booking.code}"
+    expect(page).to have_content 'Pousada Teste - Primeiro Quarto'
+    expect(page).to have_content 'Nota: 5'
+    expect(page).to have_content 'Comentário: Ótima pousada e quarto! Recomendo!'
+    expect(page).to have_content 'Resposta: Agradecemos a avaliação!'
+    expect(page).to have_content 'Pousada Teste - Segundo Quarto'
+    expect(page).to have_content 'Nota: 3'
+    expect(page).to have_content 'Comentário: Ótima pousada mas há quartos melhores.'
+    expect(page).not_to have_content 'Hóspede: Maria dos Santos'
+  end
+end
+
+describe 'Usuário vê avaliações de uma pousada' do
+  pending 'a partir da página inicial'
+    
+  pending 'e não existem avaliações'
 end
